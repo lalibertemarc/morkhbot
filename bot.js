85006714068d3c2d9c06feff05830c09d7942317
Marc Laliberté <laliberte.marc1@gmail.com>
2017-09-21 09:53:10 -0400

    added switch case

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ Bot.on('ready', () => {
 
 //change the winner of the day 
 function changeWinner(){
-	while(winnerOfTheDay=="Clyde" || winnerOfTheDay=="Morkh's Bot"){
+	while(winnerOfTheDay=="Clyde" || winnerOfTheDay=="Morkh's Bot" ){
 		winnerOfTheDay = Bot.users.random().username
 		givepoint(winnerOfTheDay);
 	}	
@@ -97,99 +97,79 @@ Bot.on("message", (message) => {
   if (message.author.id === Bot.user.id || message.author.bot) return;
   let args = message.content.split(" ").slice(1);
 
+
   //winner of the day
-  if (message.content.startsWith(prefix + "winner")) {
-    message.channel.send("The Winner of the day is "+ winnerOfTheDay + "! Gratz!!");
-  }
-  if (message.content.startsWith(prefix + "changewinner")) {
-  	if(message.author.username=="Morkh"){
-  		changeWinner();
-    	message.channel.send("The Winner of the day is now "+ winnerOfTheDay);
-  	}else{
-  		message.channel.send("You don't the permission to do that");
-  	}
-  	
-  }
+  if (message.content.startsWith(prefix)) {
+  	var cmd = message.content;
 
-  //get a point if you make a play command
-   if (message.content.startsWith(prefix + "play")) {
-    givepoint(message.author.username);
-  }
+  	switch (cmd){
 
-  //mick
-  if (message.content.startsWith(prefix + "mick")) {   
-    if(message.author.username=="Kraken"){
-  		message.channel.send("Je suis" + message.author +", et j'aime mon playstation!");
-  	}else{
-  		message.channel.send("Mick ye poche y joue a Destiny 2 sur console... lol");
-  	}
-  }
+  		case "!winner":
+	  		message.channel.send("The Winner of the day is "+ winnerOfTheDay + "! Gratz!!");
+	  		break;
 
-  //roll
-  if (message.content.startsWith(prefix + "roll")) {
-    message.channel.send(message.author.username+" has rolled "+(Math.floor(Math.random() * 100) + 1)  +"!");
-  }
+  		case "!changewinner":
+	  		if(message.author.username=="Morkh"){
+	  			changeWinner();
+	    		message.channel.send("The Winner of the day is now "+ winnerOfTheDay);
+	  		}else{
+	  			message.channel.send("You don't have the permission to do that");
+	  		}
+	  		break;
 
-  //chipchocolate
-  if (message.content.startsWith(prefix + "chipchocolate")) {
-  	if(message.author.username=="chipchocolate"){
-  		message.channel.send("Je suis" + message.author +", un dieu de pubg!");
-  	}else{
-  		message.channel.send("chipchocolate, plz carry nous a pubg!");
-  	}    
-  }
-  //join//leave general Channel
-  if(message.content.startsWith(prefix + "joinGeneral")) {  
-  	joinGeneralChannel();
-  }
-  if(message.content.startsWith(prefix + "leaveGeneral")) {  
-  	leaveGeneralChannel();
-  }
+  		//get a point if you make a play command
+    	case "!play":
+    		givepoint(message.author.username);
+    		break;
 
-  //point system
-  if(message.content.startsWith(prefix + "points")) { 
-  	var key =  message.author.username;
-  	if(userHash[key]==undefined || userHash[key]==null){
-  		givepoint(key);
-  	}
-  	if(!userHash[key]){
-  		message.channel.send(message.author+" has 0 points. RIP");
-  	}else{
-  		message.channel.send(message.author+" has "+userHash[key]+" points.");
-  	}  	
-  }
-  //gimmepoints
-  if(message.content.startsWith(prefix + "gimmepoint")) {  
-  	var key =  message.author.username;
-  	givepoint(key);
-  	message.channel.send(message.author+" has now "+userHash[key]+" points.");
+    	case "!roll":
+    		message.channel.send(message.author.username+" has rolled "+(Math.floor(Math.random() * 100) + 1)  +"!");
+    		break;
 
-  }
-  if(message.content.startsWith(prefix + "give points ")) {  
-  	var key =  message.author.username;
-  	var points = +message.content.slice(-2);
-  	givepoints(key,points);
-  	message.channel.send(message.author+" has now "+userHash[key]+" points.");
+    	case "!joinGeneral":
+    		joinGeneralChannel();
+    		break;
+    	case "!leaveGeneral":
+    		leaveGeneralChannel();
+    		break;
 
-  }
-  //save points
-  if(message.content.startsWith(prefix + "savepoints")){
-  	saveUserPoints();
-  }
-  //display all points
-  if(message.content.startsWith(prefix + "allpoints")){ 	
-  		message.channel.send(JSON.stringify(userHash));  	 	
-  }
-  //reset all apoints
-  if(message.content.startsWith(prefix + "resetpoints")){
-  	if(message.author.username=="Morkh"){
-  		resetPoints();
-  		message.channel.send(JSON.stringify(userHash));  
-  	}else{
-  		message.channel.send("You don't the permission to do that");
-  	} 	  			 	
-  }
+    	case "!points":
+    		var key =  message.author.username;
+  			if(userHash[key]==undefined || userHash[key]==null){
+  				givepoint(key);
+  			}
+  			if(!userHash[key]){
+  				message.channel.send(message.author+" has 0 points. RIP");
+  			}else{
+  				message.channel.send(message.author+" has "+userHash[key]+" points.");
+  			}
+  			break; 	
+  		
+  		case "!gimmepoint":
+  			var key =  message.author.username;
+  			givepoint(key);
+  			message.channel.send(message.author+" has now "+userHash[key]+" points.");
+  			break;
 
+  		case "!give points ":
+  			var key =  message.author.username;
+  			var points = +message.content.slice(-2); 			
+  			givepoints(key,points);
+  			message.channel.send(message.author+" has now "+userHash[key]+" points.");
+  			break;
+  		case "!allpoints":
+  			message.channel.send(JSON.stringify(userHash));
+  			break;
+  		case "!resetpoints":
+  			if(message.author.username=="Morkh"){
+  				resetPoints();
+  				message.channel.send(JSON.stringify(userHash));  
+  			}else{
+  				message.channel.send("You don't have the permission to do that");
+  			} 	  		
+
+	}
+}
 
 });
 
