833837f5e8bf4fc0083cf3b218a28196f5d5ae30
Marc Laliberté <laliberte.marc1@gmail.com>
2017-12-17 13:55:19 -0500

    corrected duel errors

--- duel.js
+++ duel.js
@@ -1,5 +1,5 @@
 var duelData ={'armed': false, "initiator": "", "target": "", "initiatorRoll": 0, "targetRoll":0}
-var launcher = require('./dicelaucher.js')
+var launcher = require('./dicelauncher.js')
 
 function duelRoll(name, d){
 	if(d==''){
@@ -20,7 +20,7 @@ function initiateDuel(initatorName, targetName){
 }
 
 function acceptDuel(targetName){
-	if(targetName==duelData['targetName']){
+	if(targetName==duelData['target']){
 		duelData['armed']=true
 	}
 }
@@ -35,10 +35,10 @@ function clearDuelData(){
 
 function rollCheck(rollerName, roll){
 	if(duelData['armed']==false) return
-	if(rollerName==duelData['initiatorName']){
+	if(rollerName==duelData['initiator']){
 		duelData['initiatorRoll']= roll
 	}
-	else if(rollerName==duelData['targetName']){
+	else if(rollerName==duelData['target']){
 		duelData['targetRoll']=roll
 	}else{
 		return "You are not being dueled right now"
@@ -49,22 +49,28 @@ function endDuel(){
 	if(duelData['armed']==false) return "There is no duel right now"
 	if(duelData['initiatorRoll'] > duelData['targetRoll']){
 		return duelData['initiator']+" wins the duel over "+ duelData['target']+" with a roll of "+
-				duelData['initatorRoll']+" over " duelData['targetRoll']+"."
+				duelData['initiatorRoll']+" over " +duelData['targetRoll']+"."
 	}
 	if(duelData['initiatorRoll'] < duelData['targetRoll']){
 		return duelData['target']+" wins the duel over "+ duelData['initiatorRoll']+" with a roll of "+
-				duelData['targetRoll']+" over " duelData['initiatorRoll']+"."
+				duelData['targetRoll']+" over " +duelData['initiatorRoll']+"."
 	}
 	if(duelData['initiatorRoll']==duelData['targetRoll']){
 		return "It's a tie! No one wins"
 	}
 	clearDuelData();
 }
-
+/*
 initiateDuel("Morkh", "Almighty2k")
+acceptDuel("Almighty2k")
 console.log(duelData)
+duelRoll("Morkh", "")
+duelRoll("Almighty2k", "")
+console.log(duelData)
+console.log(endDuel())
+*/
 
-module.exports{
+module.exports={
 	duelRoll: duelRoll,
 	initiateDuel : initiateDuel,
 	acceptDuel : acceptDuel,
