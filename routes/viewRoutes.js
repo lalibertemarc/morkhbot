c955d2e681f5f001f5d839f626e4d4efbfd4aabb
marclaliberte <mlaliberte@addenergie.com>
2021-01-01 19:22:20 -0500

    cleanup

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const Bot = require("../server.js").Bot;
 const commandList = require("./../commandList.js").commandList;
-const helpers = require("./../services/helperFunctions.js");
 
 router.get("/help", (req, res, next) => {
-  var commandResponse = [];
+    var commandResponse = [];
 
-  for (var key in commandList) {
-    var command = commandList[key];
-    commandResponse.push(`${command.name}   : ${command.description}`);
-  }
-  res.render("help", {
-    title: "Morkhbot",
-    list: commandResponse
-  });
+    for (var key in commandList) {
+        var command = commandList[key];
+        commandResponse.push(`${command.name}   : ${command.description}`);
+    }
+    res.render("help", {
+        title: "Morkhbot",
+        list: commandResponse,
+    });
 });
 
 router.get("/:view", (req, res, next) => {
-  res.render(req.params.view, { title: "Morkhbot" });
+    res.render(req.params.view, { title: "Morkhbot" });
 });
 
 module.exports = router;
