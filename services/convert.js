bdb717ff3da3a571a093e41418e4e9c2b26774ff
marclaliberte <mlaliberte@addenergie.com>
2021-01-01 19:46:10 -0500

    added unit test for convert.js + fixed bad input

--- services/convert.js
+++ services/convert.js
@@ -1,42 +1,36 @@
 const helpers = require("./helperFunctions.js");
 
-
-function dec2Bin(int){
-    if(!helpers.isNumber(int))
-        return "Given argument is not a numerical."
-	if (int==0){
+function dec2Bin(int) {
+    if (!helpers.isNumber(int)) return "Given argument is not a numerical.";
+    if (int == 0) {
         return 0;
-    }else{
-        return dec2Bin(Math.floor(int/2))+(int%2)+"";
-    }      
+    } else {
+        return dec2Bin(Math.floor(int / 2)) + (int % 2) + "";
+    }
 }
 
-function bin2Dec(string){
-    if (
-        string.includes("2") ||
-        string.includes("3") ||
-        string.includes("4") ||
-        string.includes("5") ||
-        string.includes("6") ||
-        string.includes("7") ||
-        string.includes("8") ||
-        string.includes("9")
-    )
+function bin2Dec(string) {
+    let binaryRegex = /^[0-1]*$/;
+    let match = binaryRegex.exec(string);
+    if (match == null) {
         return "ERROR: Argument is not binary";
-	somme=0
-	i=string.length-1
-	j=0
-	while(j<=string.length){
-		if(string.charAt(i)==1){
-			somme+=Math.pow(2,j)
-		}
-		j++
-		i--
-	}
-	return somme
+    } else {
+        let input = match[0];
+        somme = 0;
+        i = input.length - 1;
+        j = 0;
+        while (j <= input.length) {
+            if (input.charAt(i) == 1) {
+                somme += Math.pow(2, j);
+            }
+            j++;
+            i--;
+        }
+        return somme;
+    }
 }
 
-module.exports={
-	bin2Dec:bin2Dec,
-	dec2Bin:dec2Bin
-}
+module.exports = {
+    bin2Dec: bin2Dec,
+    dec2Bin: dec2Bin,
+};
